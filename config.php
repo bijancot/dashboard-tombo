65e4cc5ce18a9b8e6770d4b23236c47e3a564c92
bijancot <panjdia995@gmail.com>
2021-09-06 22:11:08 +0700

    refactor #2

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-define("HOST", "dash-tombo.bgskr-project.my.id");
-define("USER", "dash_tombo");
-define("PASSWORD", "1sampaitombo");
-define("DATABASE", "dash_tombo");
-
-$mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
-$koneksi = new mysqli(HOST, USER, PASSWORD, DATABASE);
-?>
